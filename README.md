5c9cb8a33b05a833668f35f22621b568b9030065
Mykola Khyliuk <nkh@ua.fm>
2023-10-24 13:50:50 +0200

    Add downgrade script for Ubuntu

---
+++ README.md
@@ -0,0 +1,9 @@
+# Intro
+A project to easily downgrade Google Chrome browser if you have experienced the bugs after it update (like flickering text, etc.).
+
+# Ubuntu
+1. Get the name of the version you are interested in from [Google Chrome Stable](https://www.ubuntuupdates.org/package/google_chrome/stable/main/base/google-chrome-stable).
+2. Paste it into the script:
+```
+sudo ./downgarade-chrome-ubuntu.sh 116.0.5845.140-1
+```
---
+++ downgarade-chrome-ubuntu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2023 https://github.com/nkh-lab
+#
+# This is free software. You can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 3 as published by the Free Software Foundation.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY.
+#
+
+CHROME_VERSION=$1
+
+if [ "$EUID" -ne 0 ]
+    then
+        echo "Please run as root, for example:"
+        echo "sudo ./downgarade-chrome-ubuntu.sh 117.0.5938.132-1"
+    exit
+fi
+
+if [ -z $CHROME_VERSION]
+    then
+        echo "Please specify version, for example:"
+        echo "sudo ./downgarade-chrome-ubuntu.sh 117.0.5938.132-1"
+    exit
+fi
+
+wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_"$CHROME_VERSION"_amd64.deb
+dpkg -i google-chrome-stable_"$CHROME_VERSION"_amd64.deb
+apt-mark hold google-chrome-stable
+google-chrome-stable --version
